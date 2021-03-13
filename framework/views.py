b8e8dbf2c08abaa3a98415a4f4af0666da8445f1
Dennis Cafiero <djc998@gmail.com>
2021-03-13 11:01:18 -0500

    Profile and Login and Registration setup completed

--- framework/views.py
+++ framework/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Framework and Site Home Page</h1>')
+    return render(request, 'framework/carousel.html', {'title': 'Home'})
--- users/views.py
+++ users/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
-#import the user account creation form
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+#need to control what logged in users can do in system
+from django.contrib.auth.decorators import login_required
+#import the custom registration form we created with fields for email etc
+from .forms import UserRegisterForm
+
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            #save the user info use built in functionality
+            form.save()
+            #lets get the data from the form
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been successfully created! You are able to login below')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
     return render(request, 'users/register.html', {'form': form})
+
+#need to use the decorator to control access to the page
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
