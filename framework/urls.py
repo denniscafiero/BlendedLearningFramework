c1e98d7c0009715412131ae581c803bd299ad02c
Dennis Cafiero <djc998@gmail.com>
2021-04-03 19:49:12 -0400

    Added Guideline Pages Need to complete Professional development and links for technology and tools.

--- framework/urls.py
+++ framework/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='framework-home'),
+    path('guidelines/course-design/', views.coursedesign, name='framework-course-design'),
+    path('guidelines/assessment/', views.assessment, name='framework-assessment'),
+    path('guidelines/activities/', views.activities, name='framework-activities'),
+    path('guidelines/technology/', views.technology, name='framework-technology'),
+    path('guidelines/professional/', views.professional, name='framework-professional')
 ]
--- framework/views.py
+++ framework/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'framework/home2.html', {'title': 'Home'})
+
+def coursedesign(request):
+    return render(request, 'framework/course-design.html', {'title': 'Course Design Guidelines'})
+
+def assessment(request):
+    return render(request, 'framework/assessment.html', {'title': 'Assessment Guidelines'})
+
+def activities(request):
+    return render(request, 'framework/activities.html', {'title': 'Guidelines for Activities'})
+
+def technology(request):
+    return render(request, 'framework/technology.html', {'title': 'Technology and Tools Guidelines'})
+
+def professional(request):
+    return render(request, 'framework/professional-development.html', {'title': 'Professional Development Guidelines'})
