67b7c826511bce9ec2e171c7a2a2092e7f4cf633
Dennis Cafiero <djc998@gmail.com>
2021-04-10 10:41:13 -0400

    New filtering for resources pages.

--- resources/urls.py
+++ resources/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
 from .views import (
+    CourseDesignResourcesListView,
+    AssessmentResourcesListView,
+    ActivitiesResourcesListView,
+    TechnologyResourcesListView,
+    ProfessionalDevelopentResourcesListView,
+    ResourceListView,
     ResourceDetailView,
     ResourceCreateView,
 )
@@ -8,12 +14,12 @@ from .views import (
 from . import views #. is current directory and we are bringing in the veiw info
 
 urlpatterns = [
-    path('', views.home, name='resource-home'),
-    path('course-design', views.CourseDesignResources, name='resource-course-design'),
-    path('assessment', views.AssessmentResources, name='resource-assessment'),
-    path('activities', views.ActivitiesResources, name='resource-activities'),
-    path('technology', views.TechnologyResources, name='resource-technology'),
-    path('professional-development', views.ProfessionalDevelopmentResources, name='resource-professional-development'),
+    path('', ResourceListView.as_view(), name='resource-home'),
+    path('course-design', CourseDesignResourcesListView.as_view(), name='resource-course-design'),
+    path('assessment', AssessmentResourcesListView.as_view(), name='resource-assessment'),
+    path('activities', ActivitiesResourcesListView.as_view(), name='resource-activities'),
+    path('technology', TechnologyResourcesListView.as_view(), name='resource-technology'),
+    path('professional-development', ProfessionalDevelopentResourcesListView.as_view(), name='resource-professional-development'),
     path('post/<int:pk>/', ResourceDetailView.as_view(), name='resource-post-detail'),
     path('post/new/', ResourceCreateView.as_view(), name='resource-post-create'),
 ]
--- resources/views.py
+++ resources/views.py
@@ -12,53 +12,130 @@ from django.views.generic import (
 from .models import Resource
 # Create your views here.
 def home(request):
+
+    #resource_info = Resource.objects.filter(framework_category='course_design')
+    resource_info = Resource.objects.all()
     #creating a dictionary for the content of the post
     context = {
-        'resources': Resource.objects.all() #takes all the posts in the database
+        'resources': resource_info #takes all the posts in the database
     }
     #passing the context to the template will allow us to access it in template
     return render(request, 'resources/home.html', context)
 
+class ResourceListView(ListView):
+    model = Resource
+    template_name = 'resources/home.html'
+    context_object_name = 'resources'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+class CourseDesignResourcesListView(ListView):
+    #resource_info = Resource.objects.filter(framework_category='course_design')
+    #creating a dictionary for the content of the post
+    #model = Resource.objects.filter(framework_category='course_design')
+    template_name = 'resources/home.html'
+    context_object_name = 'resources'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Resource.objects.filter(framework_category='course_design')
+
 def CourseDesignResources(request):
+    resource_info = Resource.objects.filter(framework_category='course_design')
     #creating a dictionary for the content of the post
     context = {
-        'resources': Resource.objects.all(), #takes all the posts in the database
+        'resources': resource_info, #takes all the posts in the database
         'filter': 'course_design'
     }
     #passing the context to the template will allow us to access it in template
     return render(request, 'resources/filter_resource.html', context)
 
+class AssessmentResourcesListView(ListView):
+    #resource_info = Resource.objects.filter(framework_category='course_design')
+    #creating a dictionary for the content of the post
+    #model = Resource.objects.filter(framework_category='course_design')
+    template_name = 'resources/home.html'
+    context_object_name = 'resources'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Resource.objects.filter(framework_category='assessment')
+
 def AssessmentResources(request):
+    resource_info = Resource.objects.filter(framework_category='assessment')
     #creating a dictionary for the content of the post
     context = {
-        'resources': Resource.objects.all(), #takes all the posts in the database
+        'resources': resource_info, #takes all the posts in the database
         'filter': 'assessment'
     }
     #passing the context to the template will allow us to access it in template
     return render(request, 'resources/filter_resource.html', context)
 
+class ActivitiesResourcesListView(ListView):
+    #resource_info = Resource.objects.filter(framework_category='course_design')
+    #creating a dictionary for the content of the post
+    #model = Resource.objects.filter(framework_category='course_design')
+    template_name = 'resources/home.html'
+    context_object_name = 'resources'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Resource.objects.filter(framework_category='activities')
+
 def ActivitiesResources(request):
+    resource_info = Resource.objects.filter(framework_category='activities')
+
     #creating a dictionary for the content of the post
     context = {
-        'resources': Resource.objects.all(), #takes all the posts in the database
+        'resources': resource_info, #takes all the posts in the database
         'filter': 'activities'
     }
     #passing the context to the template will allow us to access it in template
     return render(request, 'resources/filter_resource.html', context)
 
+class ProfessionalDevelopentResourcesListView(ListView):
+    #resource_info = Resource.objects.filter(framework_category='course_design')
+    #creating a dictionary for the content of the post
+    #model = Resource.objects.filter(framework_category='course_design')
+    template_name = 'resources/home.html'
+    context_object_name = 'resources'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Resource.objects.filter(framework_category='professional_development')
+
 def ProfessionalDevelopmentResources(request):
+    resource_info = Resource.objects.filter(framework_category='professional_development')
+
     #creating a dictionary for the content of the post
     context = {
-        'resources': Resource.objects.all(), #takes all the posts in the database
+        'resources': resource_info, #takes all the posts in the database
         'filter': 'professional_development'
     }
     #passing the context to the template will allow us to access it in template
     return render(request, 'resources/filter_resource.html', context)
 
+class TechnologyResourcesListView(ListView):
+    #resource_info = Resource.objects.filter(framework_category='course_design')
+    #creating a dictionary for the content of the post
+    #model = Resource.objects.filter(framework_category='course_design')
+    template_name = 'resources/home.html'
+    context_object_name = 'resources'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Resource.objects.filter(framework_category='technology')
+
 def TechnologyResources(request):
+    resource_info = Resource.objects.filter(framework_category='technology')
     #creating a dictionary for the content of the post
     context = {
-        'resources': Resource.objects.all(), #takes all the posts in the database
+        'resources': resource_info, #takes all the posts in the database
         'filter': 'technology'
     }
     #passing the context to the template will allow us to access it in template
